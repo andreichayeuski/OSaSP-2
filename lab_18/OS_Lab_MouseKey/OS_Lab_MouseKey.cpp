9598d07d227304b12b04c0a6a31be93474d09ec0
Andrei Chayeuski <andreichayeuski@gmail.com>
2019-05-13 09:14:23 +0300

    lab 18

---
+++ lab_18/OS_Lab_MouseKey/OS_Lab_MouseKey.cpp
@@ -0,0 +1,99 @@
+﻿// OS_Lab_MouseKey.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <windows.h>
+#include <iostream>
+  /* #define KEY_UP 72
+   #define KEY_DOWN 80
+   #define KEY_LEFT 75
+   #define KEY_RIGHT 77
+*/
+#define ENABLE_QUICK_EDIT_MODE  0x0040
+using namespace std;
+
+int main()
+{
+	HANDLE hIn;
+	HANDLE hOut;
+	COORD KeyWhere;
+	COORD MouseWhere;
+	COORD EndWhere;
+	bool Continue = TRUE;
+	int KeyEvents = 0;
+	int MouseEvents = 0;
+	INPUT_RECORD InRec;
+	DWORD NumRead;
+
+	hIn = GetStdHandle(STD_INPUT_HANDLE);
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	cout << "Key Events   : " << endl;
+	cout << "Mouse Events : " << flush;
+
+	SetConsoleMode(hIn, ENABLE_MOUSE_INPUT | ENABLE_EXTENDED_FLAGS);
+
+	KeyWhere.X = 0;
+	KeyWhere.Y = 0;
+	MouseWhere.X = 15;
+	MouseWhere.Y = 1;
+	EndWhere.X = 0;
+	EndWhere.Y = 3;
+
+	while (Continue)
+	{
+		ReadConsoleInput(hIn,
+			&InRec,
+			1,
+			&NumRead);
+
+		switch (InRec.EventType)
+		{
+		case KEY_EVENT:
+		{
+			++KeyEvents;
+			SetConsoleCursorPosition(hOut, KeyWhere);
+			cout << KeyEvents << flush;
+			if (InRec.Event.KeyEvent.uChar.AsciiChar == 'x')
+			{
+				SetConsoleCursorPosition(hOut,
+					EndWhere);
+				cout << "Exiting..." << endl;
+				Continue = FALSE;
+			}
+			switch (InRec.Event.KeyEvent.wVirtualKeyCode) {
+			case VK_UP:
+				cout << endl << "Up   " << endl;//key up
+				break;
+			case VK_DOWN:
+				cout << endl << "Down " << endl;   // key down
+				break;
+			case VK_LEFT:
+				cout << endl << "Left " << endl;  // key left
+				break;
+			case VK_RIGHT:
+				cout << endl << "Right" << endl;  // key right
+				break;
+			default:
+				cout << endl << "Mouse Events : " << endl;
+				//  cout << endl << "null" << endl;  // not arrow
+				break;
+			}
+			break;
+		}
+		case MOUSE_EVENT:
+		{
+			TCHAR* title = NULL;
+			DWORD z = GetConsoleTitle(title, sizeof(TCHAR));
+			cout << endl << InRec.Event.MouseEvent.dwMousePosition.X << endl;
+			++MouseEvents;
+			SetConsoleCursorPosition(hOut,
+				MouseWhere);
+			cout << MouseEvents << flush;
+			break;
+		}
+		}
+	}
+	getchar();
+
+	return 0;
+}
---
+++ lab_18/Task1/Task1.cpp
@@ -0,0 +1,99 @@
+﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include "pch.h"
+#include <windows.h>
+#include <iostream>
+  /* #define KEY_UP 72
+   #define KEY_DOWN 80
+   #define KEY_LEFT 75
+   #define KEY_RIGHT 77
+*/
+#define ENABLE_QUICK_EDIT_MODE  0x0040
+using namespace std;
+
+int main()
+{
+
+	HANDLE hIn;
+	HANDLE hOut;
+	COORD KeyWhere;
+	COORD MouseWhere;
+	COORD EndWhere;
+	bool Continue = TRUE;
+	int KeyEvents = 0;
+	int MouseEvents = 0;
+	INPUT_RECORD InRec;
+	DWORD NumRead;
+
+	hIn = GetStdHandle(STD_INPUT_HANDLE);
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	cout << "Key Events   : " << endl;
+	cout << "Mouse Events : " << flush;
+
+	KeyWhere.X = 0;
+	KeyWhere.Y = 0;
+	MouseWhere.X = 15;
+	MouseWhere.Y = 1;
+	EndWhere.X = 0;
+	EndWhere.Y = 3;
+	int position = 0;
+
+	SetConsoleMode(hIn, ENABLE_MOUSE_INPUT | ENABLE_EXTENDED_FLAGS);
+
+	while (Continue)
+	{
+		ReadConsoleInput(hIn,
+			&InRec,
+			1,
+			&NumRead);
+
+		switch (InRec.EventType)
+		{
+		case KEY_EVENT:
+		{
+			switch (InRec.Event.KeyEvent.wVirtualKeyCode) {
+			case VK_UP:
+				KeyWhere.Y -= 1;
+				SetConsoleCursorPosition(hOut, KeyWhere);
+				break;
+			case VK_DOWN:
+				KeyWhere.Y += 1;
+				SetConsoleCursorPosition(hOut, KeyWhere);
+				break;
+			case VK_LEFT:
+				KeyWhere.X -= 1;
+				SetConsoleCursorPosition(hOut, KeyWhere);
+				break;
+			case VK_RIGHT:
+				KeyWhere.X += 1;
+				SetConsoleCursorPosition(hOut, KeyWhere);
+				break;
+			case VK_END:
+				KeyWhere.X = position;
+				SetConsoleCursorPosition(hOut, KeyWhere);
+				break;
+			default:
+			{
+				cout << InRec.Event.KeyEvent.uChar.AsciiChar << flush;
+				KeyWhere.X += 1;
+				position = KeyWhere.X;
+				break;
+			}
+			}
+			break;
+		}
+		case MOUSE_EVENT:
+			TCHAR* title = NULL;
+			DWORD z = GetConsoleTitle(title, sizeof(TCHAR));
+			cout << endl << " X:" << InRec.Event.MouseEvent.dwMousePosition.X << " Y:" << InRec.Event.MouseEvent.dwMousePosition.Y << endl;
+			SetConsoleCursorPosition(hOut,
+				MouseWhere);
+			break;
+		}
+	}
+	getchar();
+
+	return 0;
+}
